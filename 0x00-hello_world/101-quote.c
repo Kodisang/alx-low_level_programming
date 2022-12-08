f64bde5d5d7f33ce4738d335075f72155f5f2d59
Kodisang <br.kodisang@gmail.com>
2022-12-08 09:08:30 -0800

    Task 8ight

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,14 @@
 #include <unistd.h>
 
 /**
- * *main - Entry point
- *
- * *Return: Always 1
- */
+ * main - Prints exactly and that piece of art is useful
+ * 
+ * * Return: Always 0 (Success)
+ * */
 
 int main(void)
 
 {
-		write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
