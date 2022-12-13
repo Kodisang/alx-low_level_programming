bcc83e6b85a8cc7c33cf99dd488f787cf5461485
Kodisang <br.kodisang@gmail.com>
2022-12-12 17:48:03 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,9 +20,17 @@ int main(void)
 
 		if (k < j)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
+
+		i++;
 	}
 	putchar('\n');
 
