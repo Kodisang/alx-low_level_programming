644809a1b87b50a07033d3e1252a3956d1af9808
Kodisang <br.kodisang@gmail.com>
2022-12-16 17:32:01 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,13 +1,33 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
- * Return: Always 0.
- */
-int main(void)
+* print_triangle - check for a digit
+* @size : integer type
+* Return:void
+*/
+
+void print_triangle(int size)
 {
-	print_triangle(2);
-	print_triangle(10);
-	 print_triangle(1);
-	 print_triangle(0);
+	int i = 1, ii;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+		_putchar('\n');
 }
