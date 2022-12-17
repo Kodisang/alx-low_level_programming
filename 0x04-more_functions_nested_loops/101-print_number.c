eb710bbda2b19ca8fd2c206531170056a8b1542b
Kodisang <br.kodisang@gmail.com>
2022-12-16 17:42:59 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,16 @@
 
 void print_number(int n)
 {
-	unsigned int x;
+	unsigned int i = n;
 
-	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
-		x = -n;
+		i = -i;
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
 }
